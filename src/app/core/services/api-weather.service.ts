4e74692990f594a6f00ad650b1a33c15daabfe7b
Hulyguo <karolistrinkunas8@gmail.com>
2022-05-29 13:25:04 +0300

    cleanup

--- src/app/core/services/api-weather.service.ts
+++ src/app/core/services/api-weather.service.ts
@@ -8,16 +8,6 @@ import { API_ENDPOINT, API_KEY } from 'src/app/config/api';
 @Injectable()
 export class ApiWeatherService {
   constructor(private http: HttpClient) {}
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      console.error('An error occurred:', error.error);
-    } else {
-      console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
-    }
-    return throwError(() => new Error('Something bad happened; please try again later.'));
-  }
     
   public getByCoordinates({ lat, lon }: Coordinates): Observable<WeatherResponse> {
     
